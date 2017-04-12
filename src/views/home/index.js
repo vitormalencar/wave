dbd7ab10a4abf42f36ff7f625c60eabb54425274
vitormalencar <vitor.maia.alencar@gmail.com>
2017-04-12 10:17:37 -0300

    update home page

---
+++ src/views/home/index.js
@@ -0,0 +1,11 @@
+import {h, Component} from 'preact';
+
+import AppHeader from '../../components/app-header';
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <AppHeader/>
+    );
+  }
+}
